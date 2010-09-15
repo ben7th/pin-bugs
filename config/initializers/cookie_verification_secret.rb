a214da765bdef80a246014fd6bcad4261278eae0
ben7th <ben7th@sina.com>
2010-09-15 16:52:38 +0800

    rails项目初始化

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'b3a1a2c966d596f0e1df6529dc5808bdb65ebbe509e404f61478288b921f19106f29e62f7c6fb1e709bedecac498418ce9c1eebc0907e578e773784f97b6d502';
